e7fcca777f0fb6186b8e8828cabe967f6ce7c8ad
Joona Brückner <joona.brueckner@gmail.com>
2023-05-28 02:46:36 +0200

    initial commit

---
+++ air recipe/air_recipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  air_recipeApp.swift
+//  air recipe
+//
+//  Created by Joona Brückner on 2023-05-28.
+//
+
+import SwiftUI
+
+@main
+struct air_recipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ air recipeUITests/air_recipeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  air_recipeUITestsLaunchTests.swift
+//  air recipeUITests
+//
+//  Created by Joona Brückner on 2023-05-28.
+//
+
+import XCTest
+
+final class air_recipeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
